4ba142bf15eb9f383b8fe760f0b8d3b73a5b8455
SelaxG <63264489+SelaxG@users.noreply.github.com>
2020-05-03 05:52:48 +0300

    Create main.py

---
+++ userbot/main.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2020 TeamDerUntergang.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+""" UserBot başlangıç noktası """
+
+from importlib import import_module
+from sqlite3 import connect
+from sys import argv
+
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+from . import BRAIN_CHECKER, LOGS, bot
+from .modules import ALL_MODULES
+
+DB = connect("learning-data-root.check")
+CURSOR = DB.cursor()
+CURSOR.execute("""SELECT * FROM BRAIN1""")
+ALL_ROWS = CURSOR.fetchall()
+INVALID_PH = '\nHATA: Girilen telefon numarası geçersiz' \
+             '\n  Ipucu: Ülke kodunu kullanarak numaranı gir' \
+             '\n       Telefon numaranızı tekrar kontrol edin'
+
+for i in ALL_ROWS:
+    BRAIN_CHECKER.append(i[0])
+connect("learning-data-root.check").close()
+try:
+    bot.start()
+except PhoneNumberInvalidError:
+    print(INVALID_PH)
+    exit(1)
+
+for module_name in ALL_MODULES:
+    imported_module = import_module("userbot.modules." + module_name)
+
+LOGS.info("Botunuz çalışıyor! Herhangi bir sohbete .alive yazarak Test edin."
+          " Yardıma ihtiyacınız varsa, Destek grubumuza gelin t.me/SedenUserBotSupport")
+LOGS.info("Bot sürümünüz Seden v2.0")
+
+"""
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+"""
+bot.run_until_disconnected()
